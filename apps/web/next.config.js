ea019c727bba3e94d7d7bbda5adf627beee8d1ed
Abdus Samad <imsamad00@gmail.com>
2024-08-08 05:51:24 +0530

    fixed missing filrs

---
+++ apps/web/next.config.js
@@ -0,0 +1,13 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
+module.exports = {
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    // src:https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-monorepo
+
+    // if (isServer) {
+    //   config.plugins = [...config.plugins, new PrismaPlugin()];
+    // }
+    return config;
+  },
+};
